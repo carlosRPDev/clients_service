4d5c71d2e133ce58a992a515aac4a35cb08fd071
Carlos Rodriguez <ucroxlive@gmail.com>
2025-10-20 19:46:11 -0500

    manejo de respuestas http

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class ClientsController < ApplicationController
       def index
-        render json: Client.all
+        # render json: Client.all
+        render_success(data: Client.all)
       end
 
       def show
@@ -12,27 +13,30 @@ module Api
           action: "CONSULTA_CLIENTE",
           detail: client.as_json
         )
-        render json: client
+        render_success(data: client)
       rescue ActiveRecord::RecordNotFound
         RegisterEventAuditJob.perform_later(origin_service: "clientes", action: "ERROR_CONSULTA", detail: { id: params[:id] }, state: "NOT_FOUND")
-        render json: { error: "Cliente no encontrado" }, status: :not_found
+        render_not_found("Cliente")
+      rescue => e
+        Rails.logger.error("[Clients] Error al consultar cliente: #{e.message}")
+        render_error(message: "Error al obtener cliente", status: :internal_server_error)
       end
 
       def create
         client = CreateClient.new(client_params).call
-        render json: client, status: :created
+        render_success(data: client, status: :created)
       rescue ActiveRecord::RecordInvalid => e
-          render json: e.record.errors, status: :unprocessable_entity
+        render_error(message: e.message)
       end
 
       def update
         client = Client.find(params[:id])
         if client.update(client_params)
           RegisterEventAuditJob.perform_later(origin_service: "clientes", action: "ACTUALIZAR_CLIENTE", detail: client.as_json)
-          render json: client
+          render_success(data: client, status: :ok)
         else
           RegisterEventAuditJob.perform_later(origin_service: "clientes", action: "ERROR_ACTUALIZAR_CLIENTE", detail: client.errors, state: "FAILED")
-          render json: client.errors, status: :unprocessable_entity
+          render_error(message: e.message)
         end
       end
 
